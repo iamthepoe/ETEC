b9e684d052612695e1002c462b898c214a039d54
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-03-05 16:37:17 -0300

    Delete Web Development II/BIM01/03PostMethod directory

--- Web Development II/BIM01/03PostMethod/info.php
+++
@@ -1,5 +0,0 @@
-<?php
-        echo '<marquee><h1>'.$_POST['nome'].'</h1></marque>';
-        echo '<h3>'.$_POST['assunto'].'</h3>';
-        echo '<style>body{background-color:'. $_POST['cor']. ';}</style>';
-?>
